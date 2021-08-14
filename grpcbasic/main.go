18c4ca2b1a793b4fb19dcf4cea03d6e68069c635
mahesh <mail.mahesh.lk@gmail.com>
2021-08-14 12:10:46 +0800

    basic grpc

--- grpcbasic/main.go
+++ grpcbasic/main.go
@@ -3,9 +3,19 @@ package main
 import (
 	"fmt"
 	"github.com/mahesh-dilhan/grpcbasic/who"
+	"io"
+	"net/http"
+	"net/rpc"
 )
 
 func main() {
 	w := who.NewWHO()
 	fmt.Println(w)
+	rpc.Register(w)
+
+	rpc.HandleHTTP()
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		io.WriteString(res, "server alive...")
+	})
+	http.ListenAndServe(":9999", nil)
 }
